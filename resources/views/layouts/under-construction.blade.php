0fa920c0e24c55a32c4f72a5dafa9b9e32b91ce8
Abdulbaki Adabara Suraj <codelikesuraj@gmail.com>
2022-03-15 12:22:14 +0100

    FIX: fixed typo

--- resources/views/layouts/under-construction.blade.php
+++ resources/views/layouts/under-construction.blade.php
@@ -1,7 +1,7 @@
-<header class="py-1 border-bottom">
+<header class="py-1">
     <div class="fluid-container">
         <div class="text-center border border-warning">
-            <marquee behaviour="scroll" direction="left" scrollamount="8" class="fs-3 fw-bold">⚠⛔ Site is still under construction, please don't break anything, while you are here 😁👍🏾</marquee>
+            <marquee behaviour="scroll" direction="left" scrollamount="8" class="fs-3 fw-bold">⚠️⛔ Site is still under construction, please don't break anything while you are here 😁👍🏾</marquee>
         </div>
     </div>
 </header>
