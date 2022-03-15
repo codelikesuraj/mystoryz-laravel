1885e8636c20ef3ae5a00f7391a8474c4764e3c1
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-03-15 11:09:30 +0100

    UPDATE: fixed typo

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -31,7 +31,7 @@
 		              <div class="card mb-4">
 		                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
 		                <div class="card-body">
-		                  <div class="small text-muted">{{$post->created_at->diffForHumans()}}</div>
+		                  <div class="small text-muted">Published {{$post->created_at->diffForHumans()}}</div>
 		                  <h2 class="card-title h4">{{$post->title}}</h2>
 		                  <p class="fs-6 card-text">{{ $post->excerpt }}</p>
 		                  <a class="btn btn-primary" href="{{route('post', ['post'=>$post->slug])}}">Read more →</a>
