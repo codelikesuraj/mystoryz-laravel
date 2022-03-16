974e3add39e57d908f185db943e2f6033aa53f97
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-03-16 17:56:46 +0100

    UPDATE

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,5 @@
 <x-public>
-	<x-slot name="title">Home</x-slot>
+	<x-slot name="title">{{$post->title}}</x-slot>
 	
     <!-- Under construction header -->
     @include('layouts.under-construction')
