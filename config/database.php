8d3bec921374d752a81cdd23450012243133f859
Abdulbaki Suraj <codelikesuraj@gmail.com>
2022-04-16 13:30:50 +0100

    UPDATE

--- config/database.php
+++ config/database.php
@@ -100,7 +100,7 @@ return [
     |
     | This table keeps track of all the migrations that have already run for
     | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
+    | the migrations on disk haven't actually been run in the database. 
     |
     */
 
